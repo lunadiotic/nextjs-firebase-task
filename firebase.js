fe8b43944b86142239746ed5e0cca3a6acbd6999
Lionatic <ahmadhakim404@gmail.com>
2021-11-21 15:20:43 +0700

    setup nextjs with material firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBKlODxCRScM3sxhzlf7ETuEraWOlB4Jk",
+  authDomain: "nextjs-155cd.firebaseapp.com",
+  projectId: "nextjs-155cd",
+  storageBucket: "nextjs-155cd.appspot.com",
+  messagingSenderId: "58401479027",
+  appId: "1:58401479027:web:35f87446c3fa37894d4d06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
